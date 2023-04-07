a56fcaaf71f66daa3323c961f42d71b541b4ec7e
dhanushkolluri <kolluridhanush123@gmail.com>
2023-04-07 12:29:37 +0530

    Read Employees List

---
+++ WebAPI_CRUD/WebAPI_CRUD/Controllers/EmployeeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebAPI_CRUD.Data;
+
+namespace WebAPI_CRUD.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : Controller
+    {
+        private readonly FullStackDbContext fullStackDbContext;
+
+        public EmployeeController(FullStackDbContext fullStackDbContext)
+        {
+            this.fullStackDbContext = fullStackDbContext;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAllEmployees()
+        {
+            var employees = await fullStackDbContext.Employees.ToListAsync();
+
+            return Ok(employees);
+        }
+    }
+}
